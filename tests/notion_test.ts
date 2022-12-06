ae655b2ccbd17f67653e723f3f056584267d9d76
woailizhang1 <3033244122@qq.com>
2022-12-06 17:54:51 +0800

    feat:新增代码

--- tests/notion_test.ts
+++ tests/notion_test.ts
@@ -1,6 +1,7 @@
-import { batchInsertUrlByNotion } from '/@/notion.ts';
+import { batchInsertNotion } from '/@/notion.ts';
+import 'dotenv/load.ts';
 
-const urls = Array.from(Array(100)).map(() => {
+const urls = Array.from(Array(5)).map(() => {
   return {
     name: 'test',
     url: 'http://localhost:github.com',
@@ -10,8 +11,9 @@ const urls = Array.from(Array(100)).map(() => {
 });
 
 Deno.test('batchInsertUrlByNotion', async () => {
+  Deno.env.set('database_id', '6a52381951d24603ac13ba11f0c4558c');
   try {
-    await batchInsertUrlByNotion(urls);
+    await batchInsertNotion(urls);
   } catch (e) {
     console.log(e);
     throw Error('EagleBookmarks err');
